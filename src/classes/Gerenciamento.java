ed3d467c12ba470e4d1676b0ef6a5e1bbfc76c7c
Matheus Ramos <matheusr808@gmail.com>
2023-05-02 11:03:28 -0300

    Update Gerenciamento.java

--- src/classes/Gerenciamento.java
+++ src/classes/Gerenciamento.java
@@ -6,89 +6,81 @@ import java.util.ArrayList;
 
 public class Gerenciamento {
 	List<Produtos> produtos = new ArrayList<>();
-	
+
 	public void adicionarProdutos(Produtos cp) {
-	    produtos.add(cp);
+		produtos.add(cp);
 	}
 
 	public void imprimirProdutos() {
-	    produtos.forEach((p) -> {
-	    	System.out.println("***********RELATÓRIO DO PRODUTO:**********");
-            System.out.println("Identificação: " + p.getId());
-            System.out.println("Nome: " + p.getNome());
-            System.out.println("Tamanho: " + p.getTamanho());
-            System.out.println("Cor: " + p.getCor());
-            System.out.println("Marca: " + p.getMarca());
-            System.out.println("Preço: " + p.getPreco());
-            System.out.println("Categoria: " + p.getCategoriaString(p.getCategoria()));
-            System.out.println("Fabricante: " + p.getFabricante());
-            System.out.println("Fornecedor: " + p.getFornecedor());
-            System.out.println("Quantidade: " + p.getQuantidade());
-            System.out.println("Status do Produto: " + p.getStatus());
-            System.out.println("Localidade: Matriz");
-            System.out.println("******************************************");
-	    });
+		produtos.forEach((p) -> {
+			System.out.println("***********RELATÓRIO DO PRODUTO:**********");
+			System.out.println("Identificação: " + p.getId());
+			System.out.println("Nome: " + p.getNome());
+			System.out.println("Tamanho: " + p.getTamanho());
+			System.out.println("Cor: " + p.getCor());
+			System.out.println("Marca: " + p.getMarca());
+			System.out.println("Preço: " + p.getPreco());
+			System.out.println("Categoria: " + p.getCategoriaString(p.getCategoria()));
+			System.out.println("Fabricante: " + p.getFabricante());
+			System.out.println("Fornecedor: " + p.getFornecedor());
+			System.out.println("Quantidade: " + p.getQuantidade());
+			System.out.println("Status do Produto: " + p.getStatus());
+			System.out.println("Localidade: Matriz");
+			System.out.println("******************************************");
+		});
 	}
-	
-	public void editarProdutos() {
+
+	public int buscaId(){
 		Scanner input = new Scanner(System.in);
-		
+
 		System.out.println("Informe o número do ID a ser localizado: ");
 		int busca;
 		busca = input.nextInt();
-        input.nextLine();
-		
-        int i, achou=-1;
-        System.out.println();
-        
+		input.nextLine();
+
+		int i;
+		System.out.println();
+
 		for(i = 0; i < produtos.size(); i++)
 		{
 			int idteste = produtos.get(i).getId();
 			if (idteste == busca) {
-				try {
-					produtos.get(i).editar();
-				}catch(Exception e){
-					System.out.println("Ocorreu um Erro na Leitura de 1 ou + Dados!");
-				}
-				achou=1;
-                break;
-            }
+				return i;
+			}
 		}
-		
-		if (achou==-1){
+		return (-1);
+	}
+
+	public void editarProdutos() {
+		int indice = buscaId();
+		if (indice == -1){
 			System.out.println("ID de produto não encontrado! Tente novamente");
 			System.out.println();
+		}else {
+			try {
+				produtos.get(indice).editar();
+			}catch(Exception e){
+				System.out.println("Ocorreu um Erro na Leitura de 1 ou + Dados!");
+			}
 		}
 	}
-	
+
 	public void removerProduto() {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Informe o número do ID do produto a ser removido: ");
-		int busca;
-		busca = input.nextInt();
-        input.nextLine();
-		
-        int i, achou=-1;
-        System.out.println();
-        
-		for(i = 0; i < produtos.size(); i++)
-		{
-			int idteste = produtos.get(i).getId();
-			if (idteste == busca) {
-				produtos.remove(i);
+		int indice = buscaId();
+		if (indice == -1){
+			System.out.println("ID de produto não encontrado! Tente novamente");
+			System.out.println();
+		}else {
+			try {
+				produtos.remove(indice);
 				System.out.println("Produto removido com sucesso!");
 				System.out.println();
-                achou=1;
-                break;
-            }
-		}
-		
-		if (achou==-1){
-			System.out.println("ID de produto não encontrado! Tente novamente");
-			System.out.println();
+			}catch(Exception e){
+				System.out.println("Ocorreu um Erro na Leitura de 1 ou + Dados!");
+			}
 		}
 	}
+
 	//CORREÇÃO ID
 	public Produtos correcaoId(Produtos p) {
 
