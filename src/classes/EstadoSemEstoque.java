fc4d4ba76ae84433f8f4e34d3894622992090693
Matheus Ramos <matheusr808@gmail.com>
2023-05-15 11:37:16 -0300

    Update EstadoSemEstoque.java

--- src/classes/EstadoSemEstoque.java
+++ src/classes/EstadoSemEstoque.java
@@ -1,23 +1,14 @@
 package classes;
 
-import java.util.Scanner;
+public class EstadoSemEstoque implements Estado{
 
-public class EstadoSemEstoque extends Produtos implements Estado{
-
-    public String status;
-
-    public void setStatus(String status){
-        this.status = status;
+    @Override
+    public void adicionaProduto(Produtos p) {
+        p.setStatus("Sem Estoque");
     }
 
-    public String getStatus(){
-        return status;
-    }
+    @Override
+    public void removeProduto(Produtos p) {
 
-    public void adicionar(){
-        super.adicionar ();
-        if(super.getQuantidade() == 0){
-            setStatus("Sem Estoque");//EstadoSemEstoque
-        }
     }
 }
