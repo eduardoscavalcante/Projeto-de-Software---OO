1d72020f788d09fe1e52ef0a8221ccc5cd791262
ronaldocandido99 <ronaldo.candido.96@hotmail.com>
2023-05-18 22:31:57 -0300

    update movimentação

--- src/controller/controllerMovimentacaoAdd.java
+++ src/controller/controllerMovimentacaoAdd.java
@@ -4,14 +4,36 @@ import classes.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class controllerMovimentacaoAdd {
+
+    @FXML
+    private Button adicionar;
 
     @FXML
     private Button botaoVoltar;
 
     @FXML
+    private TextField data;
+
+    @FXML
+    private TextField idProduto;
+
+    @FXML
+    private TextField idResponsavel;
+
+    @FXML
+    private TextField origem;
+
+    @FXML
+    void botaoAdicionar(ActionEvent event) {
+
+    }
+
+    @FXML
     void voltar(ActionEvent event) {
         App.changeScreen("movimentacao");
     }
+
 }
